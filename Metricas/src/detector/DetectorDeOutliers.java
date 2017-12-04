56acd1726b579445895efc443208e6e9053680b4
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2017-12-04 02:18:43 -0200

    código refatorado

--- Metricas/src/detector/DetectorDeOutliers.java
+++ Metricas/src/detector/DetectorDeOutliers.java
@@ -5,152 +5,76 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import entidades.Metrica;
 import entidades.Projeto;
+import obtedorDeMetricas.FactoryObtedorDeMetricas;
+import obtedorDeMetricas.ObtedorDeMetricas;
 
 public class DetectorDeOutliers {
-
-	private String detectarOutliersGenerico(Map<String, Projeto> projetos, List<Double> classesPorProjeto) {
-		double mediana = this.descobreMediana(classesPorProjeto);
-		double desvioAbsolutoMedio = this.getDesvioAbsolutoMedio(classesPorProjeto, mediana);
-		
-		// se for zero... https://www.ibm.com/support/knowledgecenter/en/SSWLVY_1.0.0/com.ibm.spss.analyticcatalyst.help/analytic_catalyst/modified_z.html
-		// If MAD does equal 0. Subtract the median from the score and divide by 1.253314*MeanAD. 1.253314*MeanAD approximately equals the standard deviation: (X-MED)/(1.253314*MeanAD).
-
-		if (desvioAbsolutoMedio <= 0.00001) {
-			desvioAbsolutoMedio = 1.253314 * mediana;
-		}
-
-		System.out.println("mediana: " + mediana);
-		System.out.println("desvio absoluto medio: " + desvioAbsolutoMedio);
-		System.out.println();
-		
-		List<String> outliersPositivos = this.getOutliersPositivos(mediana, desvioAbsolutoMedio, projetos);
-		List<String> outliersNegativos = this.getOutliersNegativos(mediana, desvioAbsolutoMedio, projetos);
-		
-		String retorno = this.imprimeOutliers(outliersPositivos, outliersNegativos);
-		
-		return retorno;
+	
+	private static final String LS = System.lineSeparator();
+	ObtedorDeMetricas[] obtedoresDeMetricas;
+	
+	public DetectorDeOutliers() {
+		FactoryObtedorDeMetricas factoryObtedorDeMetricas = new FactoryObtedorDeMetricas();
+		obtedoresDeMetricas = factoryObtedorDeMetricas.getObtedoresDeMetricas();
 	}
-
-	public String detectarOutliers(Map<String, Projeto> projetos) {
-		String resultado = "CLASSESSS ..... ";
-		List<Double> classesPorProjeto = this.classesPorProjeto(projetos);
-		resultado += detectarOutliersGenerico(projetos, classesPorProjeto);
-		resultado += "\n LINHASSS.....";
-		classesPorProjeto = this.linhasPorProjeto(projetos);
-		resultado += detectarOutliersGenerico(projetos, classesPorProjeto);
+	
+	public String imprimeOutliers(Map<String, Projeto> projetos) {
+		String resultado = "";
+		List<Metrica> metricas = this.detectarOutliers(projetos);
+		
+		for (Metrica metrica : metricas) {
+			resultado += metrica.toString() + LS +
+					"------------------------------" + LS + LS;
+		}
+		
 		return resultado;
 	}
-
-	private String imprimeOutliers(List<String> outliersPositivos, List<String> outliersNegativos) {
-		String lineSeparator = System.lineSeparator();
-		String retorno = "Outliers positivos: " + lineSeparator;
+	
+	private List<Metrica> detectarOutliers(Map<String, Projeto> projetos) {
+		List<Metrica> metricas = new ArrayList<>();
 		
-		for (String outlier : outliersPositivos) {
-			retorno += outlier + lineSeparator;
+		for (ObtedorDeMetricas obtedorDeMetricas : obtedoresDeMetricas) {
+			Metrica metrica = obtedorDeMetricas.obterMetricas(projetos);
+			List<Double> metricaValores = metrica.getValoresElementos();
+			
+			Double mediana = this.calculaMediana(metricaValores);
+			Double desvioAbsolutoMediano = this.getDesvioAbsolutoMediano(metricaValores, mediana);
+			
+			this.descobreOutliers(metrica, mediana, desvioAbsolutoMediano);
+			
+			metricas.add(metrica);
 		}
 		
-		retorno += lineSeparator + "Outliers negativos: " + lineSeparator;
-		
-		for (String outlier : outliersNegativos) {
-			retorno += outlier + lineSeparator;
-		}
-		
-		return retorno;
-	}
-
-	private List<String> getOutliersPositivos(double mediana, double desvioAbsolutoMedio,
-			Map<String, Projeto> projetos) {
-		
-		List<String> outliers = new ArrayList<>();
-		
-		for (String nomeDoProjeto : projetos.keySet()) {
-			Projeto projeto = projetos.get(nomeDoProjeto);
-			if (this.ehOutlierPositivo(projeto, mediana, desvioAbsolutoMedio)) {
-				outliers.add(nomeDoProjeto);
-			}
-		}
-
-		return outliers;
+		return metricas;
 	}
 	
-	private List<String> getOutliersNegativos(double mediana, double desvioAbsolutoMedio,
-			Map<String, Projeto> projetos) {
+	private void descobreOutliers(Metrica metrica, Double mediana, Double desvioAbsolutoMediano) {
+		Map<String, Double> elementos = metrica.getElementos();
 		
-		List<String> outliers = new ArrayList<>();
+		List<String> outliersPositivos = new ArrayList<>();
+		List<String> outliersNegativos = new ArrayList<>();
 		
-		for (String nomeDoProjeto : projetos.keySet()) {
-			Projeto projeto = projetos.get(nomeDoProjeto);
-			if (this.ehOutlierNegativo(projeto, mediana, desvioAbsolutoMedio)) {
-				outliers.add(nomeDoProjeto);
+		for (String nomeDoProjeto : elementos.keySet()) {
+			Double elemento = elementos.get(nomeDoProjeto);
+			
+			if (this.ehOutlierPositivo(elemento, mediana, desvioAbsolutoMediano)) {
+				outliersPositivos.add(nomeDoProjeto);
+			}
+			if (this.ehOutlierNegativo(elemento, mediana, desvioAbsolutoMediano)) {
+				outliersNegativos.add(nomeDoProjeto);
 			}
 		}
 		
-		return outliers;
-	}
-
-	private boolean ehOutlierPositivo(Projeto projeto, double mediana, double desvioAbsolutoMedio) {
-		int numeroDeClassesDoProjeto = projeto.getNumeroDeClasses();
-		
-		double zScore = (0.6745 * (numeroDeClassesDoProjeto - mediana)) / desvioAbsolutoMedio;
-		
-		return zScore > 3.5;
+		metrica.setOutliersPositivos(outliersPositivos);
+		metrica.setOutliersNegativos(outliersNegativos);
 	}
 	
-	private boolean ehOutlierNegativo(Projeto projeto, double mediana, double desvioAbsolutoMedio) {
-		int numeroDeClassesDoProjeto = projeto.getNumeroDeClasses();
-		
-		double zScore = (0.6745 * (numeroDeClassesDoProjeto - mediana)) / desvioAbsolutoMedio;
-		
-		return zScore < -3.5;
-	}
-
-	private List<Double> classesPorProjeto(Map<String, Projeto> projetos) {
-		List<Double> classesPorProjeto = new ArrayList<>();
-		
-		for (String nomeDoProjeto : projetos.keySet()) {
-			Projeto projeto = projetos.get(nomeDoProjeto);
-			double numeroDeClassesDoProjeto = projeto.getNumeroDeClasses();
-			classesPorProjeto.add(numeroDeClassesDoProjeto);
-		}
-		
-		return classesPorProjeto;
-	}
-
-	private List<Double> linhasPorProjeto(Map<String, Projeto> projetos) {
-		List<Double> classesPorProjeto = new ArrayList<>();
-		
-		for (String nomeDoProjeto : projetos.keySet()) {
-			Projeto projeto = projetos.get(nomeDoProjeto);
-			double numeroDeClassesDoProjeto = projeto.getNumeroDeLinhas();
-			classesPorProjeto.add(numeroDeClassesDoProjeto);
-		}
-		
-		return classesPorProjeto;
-	}
-
-
-	private double getDesvioAbsolutoMedio(List<Double> classesPorProjeto, double mediana) {
-		List<Double> distanciaDeCadaElementoAMediana = new ArrayList<>();
-		
-		double desvioAbsolutoMedio = 0;
-		
-		for (Double numeroDeClassesProjeto : classesPorProjeto) {
-			double distancia = Math.abs(numeroDeClassesProjeto - mediana);
-			distanciaDeCadaElementoAMediana.add(distancia);
-		}
-		
-		Collections.sort(distanciaDeCadaElementoAMediana);
-		System.out.println(distanciaDeCadaElementoAMediana);	
-		desvioAbsolutoMedio = descobreMediana(distanciaDeCadaElementoAMediana);
-		
-		return desvioAbsolutoMedio;
-	}
-
-	private double descobreMediana(List<Double> lista) {
+	private double calculaMediana(List<Double> elementos) {
 		double mediana;
 
-		List<Double> listaCopia = new ArrayList<>(lista);
+		List<Double> listaCopia = new ArrayList<>(elementos);
 		
 		Collections.sort(listaCopia);
 		
@@ -165,5 +89,41 @@ public class DetectorDeOutliers {
 		
 		return mediana;
 	}
+	
+	private double getDesvioAbsolutoMediano(List<Double> elementosPorProjeto, double mediana) {
+		double desvioAbsolutoMediano = 0;
+		
+		List<Double> distanciaDeCadaElementoAMediana = this.calculaDistanciaDeCadaElementoAMediana(elementosPorProjeto, mediana);
+		
+		desvioAbsolutoMediano = calculaMediana(distanciaDeCadaElementoAMediana);
+		
+		if (desvioAbsolutoMediano <= 0.00001) {
+			desvioAbsolutoMediano = 1.253314 * mediana;
+		}
+		
+		return desvioAbsolutoMediano;
+	}
 
+	private List<Double> calculaDistanciaDeCadaElementoAMediana(List<Double> elementosPorProjeto, double mediana) {
+		List<Double> distanciaDeCadaElementoAMediana = new ArrayList<>();
+		
+		for (Double elemento : elementosPorProjeto) {
+			double distancia = Math.abs(elemento - mediana);
+			distanciaDeCadaElementoAMediana.add(distancia);
+		}
+		
+		return distanciaDeCadaElementoAMediana;
+	}
+	
+	private boolean ehOutlierPositivo(Double elemento, double mediana, double desvioAbsolutoMediano) {		
+		double zScore = (0.6745 * (elemento - mediana)) / desvioAbsolutoMediano;
+		
+		return zScore > 3.5;
+	}
+	
+	private boolean ehOutlierNegativo(Double elemento, double mediana, double desvioAbsolutoMedio) {		
+		double zScore = (0.6745 * (elemento - mediana)) / desvioAbsolutoMedio;
+		
+		return zScore < -3.5;
+	}
 }
