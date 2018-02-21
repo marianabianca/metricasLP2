818859543b077d1673f66de5ce957b3606ccac81
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-21 15:48:58 -0300

    adicionado projeto para ser usado em testes na pasta Projetos

---
+++ Metricas/Projetos/Teste/src/package1/Classe1.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class Classe1 {
+	
+	private String atributo;
+	
+	public Classe1() {
+		this.atributo = "atributo";
+	}
+	
+	public void teste0(String teste) { }
+	
+	public void teste1() { }
+	
+	public void teste2() { 
+		
+	}
+	
+	public void teste3(String teste,
+			String teste1) { 
+		
+	}
+
+}
---
+++ Metricas/Projetos/Teste/src/package1/Classe2.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class Classe2 {
+	
+	public Classe2() {}
+
+}
