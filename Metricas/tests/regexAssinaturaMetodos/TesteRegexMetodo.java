25fa2a4deba44fb736176e71f9e4a792a27251d0
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-02-21 15:48:16 -0300

    novos testes para regex

---
+++ Metricas/tests/regexAssinaturaMetodos/TesteRegexMetodo.java
@@ -0,0 +1,36 @@
+package regexAssinaturaMetodos;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TesteRegexMetodo {
+
+	@Test
+	public void test() {
+		String assinaturaMetodo1 = "public void test()";
+		String assinaturaMetodo2 = "private void atualizarNumDeClassesProjeto(Projeto projeto, String path) {";
+		String assinaturaMetodo3 = "private void atualizarNumDeClassesProjeto(Projeto projeto";
+		String assinaturaMetodo4 = "void atualizarNumDeClassesProjeto()";
+		String assinaturaMetodo5 = "void atualizarNumDeClassesProjeto(";
+		String assinaturaMetodo6 = "void atualizarNumDeClassesProjeto() {}";
+		String assinaturaMetodo7 = "void atualizarNumDeClassesProjeto() { }";
+		String assinaturaFor = "for(i i: i) {";
+		String chamadaDeMetodo = "nome = pegarNome()";
+		
+		String regex = "[^=]*\\s*[^=]+\\s+[^=]+[(][^=]*[)]?\\s*[{]?\\s*[}]?";
+		
+		Assert.assertTrue(assinaturaMetodo1.matches(regex));
+		Assert.assertTrue(assinaturaMetodo2.matches(regex));
+		Assert.assertTrue(assinaturaMetodo3.matches(regex));
+		Assert.assertTrue(assinaturaMetodo4.matches(regex));
+		Assert.assertTrue(assinaturaMetodo5.matches(regex));
+		Assert.assertTrue(assinaturaMetodo6.matches(regex));
+		Assert.assertTrue(assinaturaMetodo7.matches(regex));
+		
+		Assert.assertFalse(assinaturaFor.matches(regex));
+		Assert.assertFalse(chamadaDeMetodo.matches(regex));
+	}
+
+}
