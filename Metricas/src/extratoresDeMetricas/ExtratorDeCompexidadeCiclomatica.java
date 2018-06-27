7db4342f1055754581581c888a37d4bc4c87a776
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-06-26 22:17:07 -0300

    Documentando

--- Metricas/src/extratoresDeMetricas/ExtratorDeCompexidadeCiclomatica.java
+++ Metricas/src/extratoresDeMetricas/ExtratorDeCompexidadeCiclomatica.java
@@ -13,6 +13,12 @@ public class ExtratorDeCompexidadeCiclomatica implements ExtratorDeMetricas {
 
 	private BufferedReader bufferedReader;
 
+	/**
+	 * Método que extrai a métrica de complexidade ciclomática do projeto
+	 * 
+	 * @param projetos - Projetos que serão medidos
+	 * @param path - Caminho até a pasta dos projetos
+	 */
 	public void extrairMetricas(Map<String, Projeto> projetos, String path) {
 		Set<String> nomesProjetos = projetos.keySet();
 
@@ -22,6 +28,12 @@ public class ExtratorDeCompexidadeCiclomatica implements ExtratorDeMetricas {
 		}
 	}
 
+	/**
+	 * Método que atualiza a complexidade ciclomática média de um projeto.
+	 * 
+	 * @param projeto - Projeto que terá a sua complexidade atualizada
+	 * @param path - Caminho até diretório dos projetos
+	 */
 	private void atualizarComplexidadeCiclomatica(Projeto projeto, String path) {
 		String nomeProjeto = projeto.getNome();
 		String pathProjeto = criarPath(path, nomeProjeto);
@@ -30,6 +42,14 @@ public class ExtratorDeCompexidadeCiclomatica implements ExtratorDeMetricas {
 		projeto.setComplexidadeCiclomatica(complexidadeCiclomatica);
 	}
 
+	/**
+	 * REMOVER ISSO AQUI
+	 * Método que busca o caminho do arquivo que contém o Java NCSS Report, 
+	 * o qual possui a métrica de complexidade ciclomática
+	 * 
+	 * @param pathProjeto - Caminho até o diretório do projeto
+	 * @return
+	 */
 	private String buscaJavancss(String pathProjeto) {
 		String[] list = pegarConteudoDoDiretorio(pathProjeto);
 		String pathReport = pathProjeto;
@@ -41,6 +61,13 @@ public class ExtratorDeCompexidadeCiclomatica implements ExtratorDeMetricas {
 		return pathReport;
 	}
 	
+	/**
+	 * Método que extrai a métrica de complexidade ciclomática média do arquivo 
+	 * 		que contém o Java NCSS Report
+	 * 
+	 * @param pathReport - Caminho do arquivo Java NCSS Report
+	 * @return Métrica CCN: Complexidade ciclomática média do projeto
+	 */
 	private double pegarComplexidadeCiclomatica(String pathReport) {
 		String ccn = "";
 		try {
@@ -68,11 +95,20 @@ public class ExtratorDeCompexidadeCiclomatica implements ExtratorDeMetricas {
 		return c;
 	}
 
+	/**
+	 * Cria o caminho até o arquivo que contém o Java NCSS Report.
+	 * @param path - Caminho para o diretório dos projetos
+	 * @param elemento - Nome do projeto do qual se deseja extrair a métrica
+	 * @return Caminho até o arquivo `javancss-raw-report.xml`
+	 */
 	private String criarPath(String path, String elemento) {
 		String retorno = path + File.separator + elemento + File.separator + "target";
 		return retorno;
 	}
 
+	/**
+	 * REMOVER ISSO AQUI TAMBÉM
+	 */
 	private String[] pegarConteudoDoDiretorio(String pathInicial) {
 		File diretorioInicial = new File(pathInicial);
 		String[] elementosDiretorio = diretorioInicial.list();
